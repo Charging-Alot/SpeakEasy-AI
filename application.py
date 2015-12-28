0c65aee31014a357f14d625808ba65d969b8bcb5
Laura Gelston <lauragelston@gmail.com>
2015-12-27 20:22:46 -0800

    async model loading

--- application.py
+++ application.py
@@ -3,20 +3,38 @@ from __future__ import absolute_import
 
 import sys
 import os
+import time
+from threading import Thread
 
 from flask import Flask, jsonify, make_response, request, abort 
 
 from MARVIN import speak_easy
 
-Marvin = speak_easy.initialize_chatbot()   
 application = Flask(__name__)
+
+Marvin = None
+print("Loading SpeakEasy AI server")
+
+def initialize():
+    global Marvin
+    print("Considering loading Marvin")
+
+    if not Marvin:
+        print("Going to load Marvin")
+        Marvin = speak_easy.initialize_chatbot()
+        print("Marvin is loaded")
 
 @application.route('/', methods=["GET"])
 def root():
-    return make_response('fizzle bizzle', 200)
+    return make_response('fizzle bizzle %s' % time.time(), 200)
 
 @application.route('/marvin', methods=["POST"])
 def generate_response():
+    if not Marvin:
+      print("Marvin is not ready yet")
+      abort(418)
+      return
+
     try:
       if not request.json or not 'prompt' in request.json:
         abort(400)
@@ -26,4 +44,7 @@ def generate_response():
       return make_response(jsonify({'error': sys.exc_info()[0]}), 500)
 
 if __name__ == '__main__':
-    application.run(debug=False)
+    initialize()
+    application.run(debug=True)
+else:
+    Thread(target=initialize).start()
